0ed70fffc7c9530f36066a7a66e7df8337fe3cd1
Mich <caedesque.apps@icloud.com>
2026-06-24 20:23:15 +0300

    Add a guard for encounter combat
    If player dies during a boss fight the timer wont stop as the fight continues

--- EventHandler.lua
+++ EventHandler.lua
@@ -5,12 +5,15 @@ addon.EventHandler = EventHandler
 
 local eventFrame = CreateFrame("Frame")
 local initialized = false
+local encounterActive = false
 
 function EventHandler:Initialize()
 	if initialized then
 		error("EventHandler already initialized", 2)
 	end
 
+	eventFrame:RegisterEvent("ENCOUNTER_START")
+	eventFrame:RegisterEvent("ENCOUNTER_END")
 	eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
 	eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
 
@@ -23,10 +26,28 @@ function EventHandler:Initialize()
 	initialized = true
 end
 
+function EventHandler:ENCOUNTER_START()
+	encounterActive = true
+	addon.CombatTimer:Start()
+end
+
+function EventHandler:ENCOUNTER_END()
+	encounterActive = false
+	addon.CombatTimer:Stop()
+end
+
 function EventHandler:PLAYER_REGEN_DISABLED()
+	if encounterActive then
+		return
+	end
+
 	addon.CombatTimer:Start()
 end
 
 function EventHandler:PLAYER_REGEN_ENABLED()
+	if encounterActive then
+		return
+	end
+
 	addon.CombatTimer:Stop()
 end
